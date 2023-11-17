35de64b4058fa0542999612955edbfcdfb05f043
Marcus Jensen <mmj@dnp.dk>
2023-11-17 10:32:53 +0100

    Updated notifications

--- js/notifications.js
+++ js/notifications.js
@@ -1,26 +1,67 @@
-Notification.requestPermission().then((result) => {
-    if (result === "granted") {
-        randomNotification();
+const registration = await navigator.serviceWorker.getRegistration();
+
+const sendNotification = async () => {
+    if (Notification.permission === 'granted') {
+        showNotification(notification.value);
+    } else {
+        if (Notification.permission !== 'denied') {
+            const permission = await Notification.requestPermission();
+
+            if (permission === 'granted') {
+                showNotification(notification.value);
+            }
+        }
     }
-});
+};
 
-function randomNotification() {
-    const randomItem = Math.floor(Math.random() * games.length);
-    const notifTitle = games[randomItem];
-    const notifBody = `Created by tsgsOFFICIAL.`;
-    const notifImg = `./assets/icons/128x128.png`;
-    const options = {
-        body: notifBody,
-        icon: notifImg,
+const showNotification = (notifyBody, notifyImg = `./assets/icons/128x128.png`) => {
+    const notifyTitle = 'What PWA Can Do Today';
+
+    const payload = {
+        body: notifyBody,
+        icon: notifyImg,
     };
-    new Notification(notifTitle, options);
-    setTimeout(randomNotification, 30000);
-}
 
-const games = [
-    "CS2",
-    "CS:GO",
-    "PUBG",
-    "Stranded Deep",
-    "Viscera Cleanup Detail"
-];
+    if ('showNotification' in registration) {
+        registration.showNotification(notifyTitle, payload);
+    }
+    else {
+        new Notification(notifyTitle, payload);
+    }
+};
+
+
+
+// Notification.requestPermission().then((result) => {
+//     if (result === "granted") {
+//         randomNotification();
+//     }
+// });
+
+// function randomNotification() {
+//     const randomItem = Math.floor(Math.random() * games.length);
+//     const notifTitle = games[randomItem];
+//     const notifBody = `Created by tsgsOFFICIAL.`;
+//     const notifImg = `./assets/icons/128x128.png`;
+// const payload = {
+//     body: notifBody,
+//     icon: notifImg,
+// };
+
+//     if ('showNotification' in registration) {
+//         registration.showNotification(notifTitle, payload);
+//     }
+//     else {
+//         new Notification(notifTitle, payload);
+//     }
+
+//     setTimeout(randomNotification, 30000);
+// }
+
+// const games = [
+//     "CS2",
+//     "CS:GO",
+//     "PUBG",
+//     "Stranded Deep",
+//     "Viscera Cleanup Detail"
+// ];
