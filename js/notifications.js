324a966601b419d2467737e6723c8113836b8b15
Marcus Jensen <mmj@dnp.dk>
2023-11-17 10:37:00 +0100

    Updated notifications

--- js/notifications.js
+++ js/notifications.js
@@ -1,4 +1,10 @@
-const registration = await navigator.serviceWorker.getRegistration();
+let registration;
+
+topLevelFunction();
+
+async function topLevelFunction() {   
+    registration = await navigator.serviceWorker.getRegistration();
+}
 
 const sendNotification = async () => {
     if (Notification.permission === 'granted') {
@@ -6,7 +12,7 @@ const sendNotification = async () => {
     } else {
         if (Notification.permission !== 'denied') {
             const permission = await Notification.requestPermission();
-
+            
             if (permission === 'granted') {
                 showNotification(notification.value);
             }
@@ -16,12 +22,12 @@ const sendNotification = async () => {
 
 const showNotification = (notifyBody, notifyImg = `./assets/icons/128x128.png`) => {
     const notifyTitle = 'What PWA Can Do Today';
-
+    
     const payload = {
         body: notifyBody,
         icon: notifyImg,
     };
-
+    
     if ('showNotification' in registration) {
         registration.showNotification(notifyTitle, payload);
     }
@@ -33,24 +39,24 @@ const showNotification = (notifyBody, notifyImg = `./assets/icons/128x128.png`) 
 
 
 // Notification.requestPermission().then((result) => {
-//     if (result === "granted") {
-//         randomNotification();
-//     }
-// });
-
-// function randomNotification() {
-//     const randomItem = Math.floor(Math.random() * games.length);
-//     const notifTitle = games[randomItem];
-//     const notifBody = `Created by tsgsOFFICIAL.`;
-//     const notifImg = `./assets/icons/128x128.png`;
-// const payload = {
-//     body: notifBody,
-//     icon: notifImg,
-// };
-
-//     if ('showNotification' in registration) {
-//         registration.showNotification(notifTitle, payload);
-//     }
+    //     if (result === "granted") {
+        //         randomNotification();
+        //     }
+        // });
+        
+        // function randomNotification() {
+            //     const randomItem = Math.floor(Math.random() * games.length);
+            //     const notifTitle = games[randomItem];
+            //     const notifBody = `Created by tsgsOFFICIAL.`;
+            //     const notifImg = `./assets/icons/128x128.png`;
+            // const payload = {
+                //     body: notifBody,
+                //     icon: notifImg,
+                // };
+                
+                //     if ('showNotification' in registration) {
+                    //         registration.showNotification(notifTitle, payload);
+                    //     }
 //     else {
 //         new Notification(notifTitle, payload);
 //     }
@@ -59,9 +65,9 @@ const showNotification = (notifyBody, notifyImg = `./assets/icons/128x128.png`) 
 // }
 
 // const games = [
-//     "CS2",
-//     "CS:GO",
-//     "PUBG",
-//     "Stranded Deep",
-//     "Viscera Cleanup Detail"
-// ];
+    //     "CS2",
+    //     "CS:GO",
+    //     "PUBG",
+    //     "Stranded Deep",
+    //     "Viscera Cleanup Detail"
+    // ];
